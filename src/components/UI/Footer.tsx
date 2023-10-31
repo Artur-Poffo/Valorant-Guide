a7632697493ada97d5587bb9df3ebc90d64a177c
Artur Poffo <arturpoffoc@hotmail.com>
2023-10-31 10:00:09 -0300

    Update: Footer

--- src/components/UI/Footer.tsx
+++ src/components/UI/Footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export function Footer() {
     return (
-        <footer className="w-full p-4 bg-brand-gray-800 flex justify-center" >
+        <footer className="w-full py-5 bg-brand-gray-800 flex justify-center" >
             <Link className="text-brand-white-300 underline underline-offset-4 transition-opacity hover:opacity-70" href={"https://valorant-guide.vercel.app"} >Código fonte no GitHub</Link>
         </footer>
     )
